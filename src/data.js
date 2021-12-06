cf45cf075c62e071762bc61a63a1203e51f5eb7a
johnsmccain <johnsdanlami@mail.com>
2021-12-06 23:54:34 +0000

    add new project

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 import resume from "./img/resume.png";
 import landing from "./img/landing.png";
 import quiz from "./img/quiz.png";
+import netflix from "./img/netflix-clone.png";
 
 export const product = [
   {
@@ -18,11 +19,11 @@ export const product = [
     img: quiz,
     link: "https://61a1a959bcaeb60008567cbc--millionairesgame.netlify.app/",
   },
-  //   {
-  //     id: 4,
-  //     img: long4,
-  //     link: "ahjabajs",
-  //   },
+  {
+    id: 4,
+    img: netflix,
+    link: "https://netflix-repl.netlify.app",
+  },
   //   {
   //     id: 5,
   //     img: long5,
